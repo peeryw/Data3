73e5bd62e715379de2071a13412ab46f6aeaab04
Scott Peery <william.scott.peery@gmail.com>
2016-07-18 11:48:04 -0500

    created solution folder and skeleton main.cpp

---
+++ Project3 CS303/Project3 CS303/Main.cpp
@@ -0,0 +1,27 @@
+/*
+Name:	Jack Goza, Joshua Ford, Scott Peery
+Course:	CS303
+Program:	Project 3
+Due Date:
+
+Description: 
+
+Inputs: 
+
+Outputs: 
+
+Algorithm:
+
+*****SPECIAL NOTE*****
+extra credit for using GitHub
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	system("pause");
+	return 0;
+}
