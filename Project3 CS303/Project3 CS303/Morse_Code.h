104cde2767887c0a5d5cabbc9606aca9dd3a8081
Scott Peery <william.scott.peery@gmail.com>
2016-07-21 21:29:25 -0500

    corrected morse_code.hpp indef/define from book_hpp to Morse_Code

--- Project3 CS303/Project3 CS303/Morse_Code.h
+++ Project3 CS303/Project3 CS303/Morse_Code.h
@@ -18,30 +18,8 @@ the assumtion is that each line will be a single string of either a single chara
 */
 
 
-/*
-
-So I think we need to change a few things moving forward.
- 
- 1) Whoever set this up, well done. Props to you, tree was bomb and your functions
-	are excellent.
- 2) With that said, we have this lean, mean tree machine ready to populate.
-	Let's make it just ridiculously OP.
- 3) We should create one tree object and then use the overloaded operator to 
-	populate its branches. Basically, the constructor creates the tree,
-	then everything else is secondary.
- 4) We can create nodes with bullshit strings and then populate based on
-	where our dotdash takes us.
- 5) Tree creation will be a function, and then we have it and we're done. Like
-	lights out, that's it.
- 6) It will have to be called first thing in main so that we create the tree
-	he wants. That's fine, it's miniscule finite runtime.
- 7) getLetter and getCode will have to be POST creation. We will have to call
-	the operator before they can be used.
- 
-*/
-
-#ifndef book_hpp
-#define book_hpp
+#ifndef Morse_Code
+#define Morse_Code
 
 #include<iostream>
 #include<string>
